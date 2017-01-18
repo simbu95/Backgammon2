8dd4e11cc1fd1e3b15681b74b55b0df4003d3b9e
simbu95 <simbu.tanksley@yahoo.com>
2017-01-18 09:27:37 -0600

    Updates
    Edited the file so that the doubling is easier to access and more
    convient to use.

--- BackGammon2.java
+++ BackGammon2.java
@@ -15,38 +15,42 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class BackGammon2 {
-    public static JTextArea area2= new JTextArea();
-    public static int location=0;
-    public static JTextField to=new JTextField("0");
-    public static JTextField from=new JTextField("0");
-    public static boolean turn=true;
-    public static JLabel turnlab=new JLabel("Player O");
-    public static JLabel dice=new JLabel("0,0,0,0");
-    public static JLabel cube=new JLabel("1");
-    public static JTextArea area= new JTextArea();
-    public static Board Gameboard=new Board();
-    public static ArrayList<Board> BoardSave=new ArrayList();
-    public static int doublestate=0;
-    public static boolean POAI=true;
-    public static boolean PXAI=false;
+
+    public static JTextArea area2 = new JTextArea();
+    public static int location = 0;
+    public static JTextField to = new JTextField("0");
+    public static JTextField from = new JTextField("0");
+    public static boolean turn = true;
+    public static JLabel turnlab = new JLabel("Player O");
+    public static JLabel dice = new JLabel("0,0,0,0");
+    public static JLabel cube = new JLabel("1");
+    public static JTextArea area = new JTextArea();
+    public static Board Gameboard = new Board();
+    public static ArrayList<Board> BoardSave = new ArrayList();
+    public static int doublestate = 0;
+    public static boolean POAI = true;
+    public static boolean PXAI = false;
+
     public static void main(String[] args) {
-        JFrame frame= new JFrame();
-        JFrame frame2= new JFrame();
-        JPanel pan=new JPanel(new GridLayout(1,2));     
-        JPanel pan2=new JPanel(new GridLayout(1,3)); 
-        area.setPreferredSize(new Dimension(350,250));
+        POAI = 0 == JOptionPane.showConfirmDialog(null, "Is Player O an AI?", "AI", JOptionPane.YES_NO_OPTION);
+        PXAI = 0 == JOptionPane.showConfirmDialog(null, "Is Player X an AI?", "AI", JOptionPane.YES_NO_OPTION);
+        JFrame frame = new JFrame();
+        JFrame frame2 = new JFrame();
+        JPanel pan = new JPanel(new GridLayout(1, 2));
+        JPanel pan2 = new JPanel(new GridLayout(1, 3));
+        area.setPreferredSize(new Dimension(350, 250));
         area.setEditable(false);
-        area2.setPreferredSize(new Dimension(350,250));
+        area2.setPreferredSize(new Dimension(350, 250));
         area2.setEditable(false);
         pan.add(area);
         pan2.add(area2);
-        JButton but1=new JButton("Left");
+        JButton but1 = new JButton("Left");
         but1.addActionListener(act2());
-        JButton but2=new JButton("Right");
+        JButton but2 = new JButton("Right");
         but2.addActionListener(act3());
         pan2.add(but1);
         pan2.add(but2);
-        Font ft=new Font("monospaced",Font.PLAIN,12);
+        Font ft = new Font("monospaced", Font.PLAIN, 12);
         area.setFont(ft);
         area.setText(Gameboard.displayBoard());
         area2.setFont(ft);
@@ -60,41 +64,45 @@ public class BackGammon2 {
         frame2.pack();
         frame2.setVisible(true);
         frame.setVisible(true);
-        while(Gameboard.getDie1()==Gameboard.getDie2()){
-        Gameboard.rollDie();
+        while (Gameboard.getDie1() == Gameboard.getDie2()) {
+            Gameboard.rollDie();
         }
         BoardSave.add(new Board(Gameboard));
-        dice.setText(Gameboard.getDie1()+","+Gameboard.getDie2()+","+Gameboard.getDie3()+","+Gameboard.getDie4()); 
-        if(Gameboard.getDie1()>Gameboard.getDie2()){
+        dice.setText(Gameboard.getDie1() + "," + Gameboard.getDie2() + "," + Gameboard.getDie3() + "," + Gameboard.getDie4());
+        if (Gameboard.getDie1() > Gameboard.getDie2()) {
             Gameboard.nextturn();
-            turn=!turn;
+            turn = !turn;
             turnlab.setText("Player X");
         }
         //AIGO();
     }
-    public static void init(){
-        turn=true;
-        BoardSave=new ArrayList();
-        Gameboard=new Board();
-        while(Gameboard.getDie1()==Gameboard.getDie2()){
-        Gameboard.rollDie();
+
+    public static void init() {
+        turn = true;
+        BoardSave = new ArrayList();
+        Gameboard = new Board();
+        doublestate = 0;
+        while (Gameboard.getDie1() == Gameboard.getDie2()) {
+            Gameboard.rollDie();
         }
         BoardSave.add(new Board(Gameboard));
-        if(Gameboard.getDie1()>Gameboard.getDie2()){
+        turnlab.setText("Player O");
+        if (Gameboard.getDie1() > Gameboard.getDie2()) {
             Gameboard.nextturn();
-            turn=!turn;
+            turn = !turn;
             turnlab.setText("Player X");
         }
     }
-    public static JPanel CreatePanel(){
-        JPanel pan=new JPanel(new GridLayout(5,2));
-        JPanel pan2=new JPanel(new GridLayout(2,1));
-        JButton but=new JButton("Move");
-        JLabel label=new JLabel("To");
-        JLabel label2=new JLabel("From");
-         JLabel label3=new JLabel("dice");
-         JLabel label5=new JLabel("Doubling Cube");
-        JLabel label8=new JLabel("It is your turn");
+
+    public static JPanel CreatePanel() {
+        JPanel pan = new JPanel(new GridLayout(5, 2));
+        JPanel pan2 = new JPanel(new GridLayout(2, 1));
+        JButton but = new JButton("Move");
+        JLabel label = new JLabel("To");
+        JLabel label2 = new JLabel("From");
+        JLabel label3 = new JLabel("dice");
+        JLabel label5 = new JLabel("Doubling Cube");
+        JLabel label8 = new JLabel("It is your turn");
         pan.add(turnlab);
         pan.add(label8);
         pan.add(label3);
@@ -110,143 +118,188 @@ public class BackGammon2 {
         but.addActionListener(act());
         return pan2;
     }
-    public static void endturncheck(){
+
+    public static void endturncheck() {
         GameEnd();
         if (Gameboard.turnover()) {
             turn = !turn;
-            if (turn) {turnlab.setText("Player O");} 
-            else turnlab.setText("Player X");
+            if (turn) {
+                turnlab.setText("Player O");
+            } else {
+                turnlab.setText("Player X");
+            }
             Doubled();
             Gameboard.rollDie();
             BoardSave.add(new Board(Gameboard));
-            dice.setText(Gameboard.getDie1()+","+Gameboard.getDie2()+","+Gameboard.getDie3()+","+Gameboard.getDie4());
+            dice.setText(Gameboard.getDie1() + "," + Gameboard.getDie2() + "," + Gameboard.getDie3() + "," + Gameboard.getDie4());
         }
-        while(!Gameboard.canbearOff() && !Gameboard.canmove()){
-            if(turn){
-                JOptionPane.showMessageDialog(null, "Player O, you do not have a valid move, your turn is over.");}
-            else JOptionPane.showMessageDialog(null, "Player X, you do not have a valid move, your turn is over.");
-                turn=!turn;
-                Gameboard.nextturn();
-                Doubled();
-                Gameboard.rollDie();
-                BoardSave.add(new Board(Gameboard));
-                dice.setText(Gameboard.getDie1()+","+Gameboard.getDie2()+","+Gameboard.getDie3()+","+Gameboard.getDie4());
+        while (!Gameboard.canbearOff() && !Gameboard.canmove()) {
+            if (turn) {
+                JOptionPane.showMessageDialog(null, "Player O, you do not have a valid move, your turn is over.");
+            } else {
+                JOptionPane.showMessageDialog(null, "Player X, you do not have a valid move, your turn is over.");
             }
-        if (turn) {turnlab.setText("Player O");} 
-        else turnlab.setText("Player X");
+            turn = !turn;
+            Gameboard.nextturn();
+            Doubled();
+            Gameboard.rollDie();
+            BoardSave.add(new Board(Gameboard));
+            dice.setText(Gameboard.getDie1() + "," + Gameboard.getDie2() + "," + Gameboard.getDie3() + "," + Gameboard.getDie4());
+        }
+        if (turn) {
+            turnlab.setText("Player O");
+        } else {
+            turnlab.setText("Player X");
+        }
     }
-    public static ActionListener act2(){
-        ActionListener action=(ActionEvent e) -> {
-            if(location>0){
+
+    public static ActionListener act2() {
+        ActionListener action = (ActionEvent e) -> {
+            if (location > 0) {
                 location--;
-                area2.setText(BoardSave.get(location).displayBoard()+location);
+                area2.setText(BoardSave.get(location).displayBoard() + location);
             }
-        };return action;}
-    public static ActionListener act3(){
-        ActionListener action=(ActionEvent e) -> {
-            if(location<BoardSave.size()-1){
+        };
+        return action;
+    }
+
+    public static ActionListener act3() {
+        ActionListener action = (ActionEvent e) -> {
+            if (location < BoardSave.size() - 1) {
                 location++;
-                area2.setText(BoardSave.get(location).displayBoard()+location);
+                area2.setText(BoardSave.get(location).displayBoard() + location);
             }
-        };return action;}
-public static ActionListener act(){
-        ActionListener action=(ActionEvent e) -> {
-            if(turn && POAI){
-                Gameboard=AI(Gameboard);
-            }
-            else if(!turn && PXAI){
-                Gameboard=AI(Gameboard);
-            }
-            else{
+        };
+        return action;
+    }
+
+    public static ActionListener act() {
+        ActionListener action = (ActionEvent e) -> {
+            if (turn && POAI) {
+                Gameboard = AI(Gameboard);
+            } else if (!turn && PXAI) {
+                Gameboard = AI(Gameboard);
+            } else {
                 Gameboard.move(Integer.parseInt(from.getText()), Integer.parseInt(to.getText()));
             }
-            dice.setText(Gameboard.getDie1()+","+Gameboard.getDie2()+","+Gameboard.getDie3()+","+Gameboard.getDie4());
+            dice.setText(Gameboard.getDie1() + "," + Gameboard.getDie2() + "," + Gameboard.getDie3() + "," + Gameboard.getDie4());
             area.setText(Gameboard.displayBoard());
             endturncheck();
         };
         return action;
     }
-    public static void Doubled(){
-        if(doublestate==0){
-            if(POAI){}
-            else if(JOptionPane.showConfirmDialog(null,"Player O, would you like to double","Double Cube",JOptionPane.YES_NO_OPTION)==0){
-                doublestate=2;
-                Gameboard.doubledown();
-                cube.setText(String.valueOf(Gameboard.getCube()));
-                return;
-            }
-            if(PXAI){}
-            else if(JOptionPane.showConfirmDialog(null,"Player X, would you like to double","Double Cube",JOptionPane.YES_NO_OPTION)==0){
-                doublestate=1;
-                Gameboard.doubledown();
-                cube.setText(String.valueOf(Gameboard.getCube()));
-                return;
-            }
-        }
-        else if(doublestate==1){
-            if(POAI){}
-            else if(JOptionPane.showConfirmDialog(null,"Player O, would you like to double","Double Cube",JOptionPane.YES_NO_OPTION)==0){
-                doublestate=2;
-                Gameboard.doubledown();
-                cube.setText(String.valueOf(Gameboard.getCube()));
-                return;
-            }
-        }
-        else
-        {
-            if(PXAI){}
-            else if(JOptionPane.showConfirmDialog(null,"Player X, would you like to double","Double Cube",JOptionPane.YES_NO_OPTION)==0){
-                doublestate=1;
-                Gameboard.doubledown();
-                cube.setText(String.valueOf(Gameboard.getCube()));
-                return;
-            }
+
+    public static void DoubleX() {
+        if (PXAI && DoubledCheck()) {
+            doublestate = 1;
+            Gameboard.doubledown();
+            cube.setText(String.valueOf(Gameboard.getCube()));
+        } else if (!PXAI && JOptionPane.showConfirmDialog(null, "Player X, Do you accept the double", "Double Cube", JOptionPane.YES_NO_OPTION) == 0) {
+            doublestate = 1;
+            Gameboard.doubledown();
+            cube.setText(String.valueOf(Gameboard.getCube()));
+        } else {
+            JOptionPane.showMessageDialog(null, "Player O has won " + Gameboard.doublecube + " games");
+            init();
         }
     }
-    
-    public static Board AI(Board g){
-        if(!g.canmove() && !g.canbearOff()){
+
+    public static void DoubleO() {
+        if (POAI && DoubledCheck()) {
+            doublestate = 2;
+            Gameboard.doubledown();
+            cube.setText(String.valueOf(Gameboard.getCube()));
+        } else if (!POAI && JOptionPane.showConfirmDialog(null, "Player O, Do you accept the double", "Double Cube", JOptionPane.YES_NO_OPTION) == 0) {
+            doublestate = 2;
+            Gameboard.doubledown();
+            cube.setText(String.valueOf(Gameboard.getCube()));
+        } else {
+            JOptionPane.showMessageDialog(null, "Player X has won " + Gameboard.doublecube + " games");
+            init();
+        }
+    }
+
+    public static void Doubled() {
+        switch (doublestate) {
+            case 0:
+                if (POAI && DoubleCheck()) {
+                    DoubleX();
+                } else if (!POAI && JOptionPane.showConfirmDialog(null, "Player O, would you like to double", "Double Cube", JOptionPane.YES_NO_OPTION) == 0) {
+                    DoubleX();
+                } else if (PXAI && DoubleCheck()) {
+                    DoubleO();
+                } else if (!PXAI && JOptionPane.showConfirmDialog(null, "Player X, would you like to double", "Double Cube", JOptionPane.YES_NO_OPTION) == 0) {
+                    DoubleO();
+                }
+                break;
+            case 1:
+                if (PXAI && DoubleCheck()) {
+                    DoubleO();
+                } else if (!PXAI && JOptionPane.showConfirmDialog(null, "Player X, would you like to double", "Double Cube", JOptionPane.YES_NO_OPTION) == 0) {
+                    DoubleO();
+                }
+                break;
+            case 2:
+                if (POAI && DoubleCheck()) {
+                    DoubleX();
+                } else if (!POAI && JOptionPane.showConfirmDialog(null, "Player O, would you like to double", "Double Cube", JOptionPane.YES_NO_OPTION) == 0) {
+                    DoubleX();
+                }
+                break;
+        }
+    }
+
+    public static Board AI(Board g) {
+        if (!g.canmove() && !g.canbearOff()) {
             return g;
         }
-        ArrayList<Board> b=new ArrayList();
-        ArrayList<MoveCont> ml=g.Movecalc();
-        for(MoveCont m:ml){
-            Board temp=new Board(g);
+        ArrayList<Board> b = new ArrayList();
+        ArrayList<MoveCont> ml = g.Movecalc();
+        for (MoveCont m : ml) {
+            Board temp = new Board(g);
             temp.move(m.getFrom(), m.getTo());
             b.add(AI(temp));
         }
         int tempe;
-        int x=0;
-        if(b.isEmpty()){g.print();}
-        else{
-        int maxscore=b.get(0).ReturnScore();
-        if(turn){
-        for(int i=1; i<b.size();i++){
-            tempe=b.get(i).ReturnScore();
-            if(tempe>maxscore){
-                x=i;maxscore=tempe;
+        int x = 0;
+        if (b.isEmpty()) {
+            g.print();
+        } else {
+            int maxscore = b.get(0).ReturnScore();
+            if (turn) {
+                for (int i = 1; i < b.size(); i++) {
+                    tempe = b.get(i).ReturnScore();
+                    if (tempe > maxscore) {
+                        x = i;
+                        maxscore = tempe;
+                    }
+                }
+            } else {
+                for (int i = 1; i < b.size(); i++) {
+                    tempe = b.get(i).ReturnScore();
+                    if (tempe < maxscore) {
+                        x = i;
+                        maxscore = tempe;
+                    }
+                }
             }
+            return b.get(x);
         }
-        }
-        else {
-            for(int i=1; i<b.size();i++){
-            tempe=b.get(i).ReturnScore();
-            if(tempe<maxscore){
-                x=i;maxscore=tempe;
-            }
-        }
-        }
-        return b.get(x);
-    }
         return g;
     }
-    
-public static void GameEnd(){
-    if(Gameboard.GameOver()){
-        init();
+
+    public static void GameEnd() {
+        if (Gameboard.GameOver()) {
+            init();
+        }
     }
+
+    public static boolean DoubledCheck() { //This decides if the AI should accept or reject a double
+        return true;
+    }
+
+    public static boolean DoubleCheck() { //This decides if the AI should double right now
+        return false;
+    }
+
 }
-
-
-}
-
